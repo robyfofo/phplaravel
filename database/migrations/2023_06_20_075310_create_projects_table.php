cbc0fe53b2b5d3280718e9b4c7154d3fdbc64948
Roberto <robyfofo@gmail.com>
2023-06-20 10:11:18 +0200

    agg

---
+++ database/migrations/2023_06_20_075310_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+
+            $table->mediumInteger('status')->default(0);
+            $table->mediumInteger('completato')->default(0);
+            
+            $table->float('costo_orario')->nullable();
+
+            $table->mediumInteger('ordering')->default(0);
+            $table->integer('active')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
